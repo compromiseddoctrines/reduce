78bef4260aa4d9fb6de6770210093d476da8743d
jonmateo <compromiseddoctrines@gmail.com>
2020-11-01 21:18:55 +0800

    reducers module two practice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,23 @@
-// Action Creator
+import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const selectSong = (song) => {
+export const fetchPosts = () => {
+    return async (dispatch) => {
+        const response = await jsonPlaceholder.get('/posts');
+
+        // return {
+        //     type: 'FETCH_POST',
+        //     payload: response
+        // }
+
+        dispatch({
+            type:'FETCH_POSTS',
+            payload: response
+        })
+    }
+}
+
+export const selectPost = () => {
     return{
-        type: 'SONG_SELECTED',
-        payload: song
-    };
-};
-
+        type: 'SELECT_POST'
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,6 @@
 import { combineReducers } from 'redux';
-
-
-const songsReducer = () => {
-    return[
-        { title: 'This is What if Feels Like', author: 'Armin Van Buuren' , duration: '5:25'},
-        { title: 'Royal Intense', author: 'Armin Van Buuren', duration: '4:24'},
-        { title: 'Tomorrowland 2015', author: 'Tomorrowland', duration: '53:29'},
-        { title: 'Something Real', author: 'Armin Van Buuren', duration: '3:58'},
-        { title: 'Remei', author: 'My First Story', duration: '4:40'}
-    ]
-};
-
-const selectedSongReducer = (selectedSong=null, action) => {
-    if(action.type === 'SONG_SELECTED'){
-        return action.payload
-    }
-
-    return selectedSong;
-};
+import postsReducer from './postsReducer';
 
 export default combineReducers({
-    songs: songsReducer,
-    selectedSong: selectedSongReducer
+    posts: postsReducer
 });
